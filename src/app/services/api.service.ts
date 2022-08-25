e02354080fea27a5f1a81a57804d3f8530a1744b
BiancaPicali <61015059+BiancaPicali@users.noreply.github.com>
2022-08-25 12:27:05 -0300

    object
    interpolação

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private url = 'https://api.openweathermap.org/data/2.5/weather?lat=-22.59861&lon=-48.80028&lang=pt_br&appid=5c0208d063ec187d9c2a78a6920ac4f2&units=metric'
+
+  constructor(private http: HttpClient) { }
+  buscarPrevisao(){
+    return this.http.get(this.url);
+  }
+}
